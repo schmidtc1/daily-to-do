6e8e07f7ff9e49e55da2f89dc8c5a4efa8f7067a
Christopher Schmidt <schmidtc086@outlook.com>
2024-02-04 18:42:19 -0500

    Creates action listeners, reorganizes code

--- src/gui.java
+++ src/gui.java
@@ -1,35 +1,46 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
-public class gui{
-    public static void main(String args[]) {
-        JFrame frame = new JFrame("GUI");
+public class gui extends Frame implements ActionListener {
+
+    private JMenuBar mb;
+
+    private JFrame frame;
+
+    private JPanel panel;
+    private JPanel date;
+    private JPanel lp;
+    private JPanel list;
+
+    private void buildWindow() {
+        frame = new JFrame("GUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 400);
+    }
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-
-        JMenuBar mb = new JMenuBar();
+    private void buildMenu() {
+        mb = new JMenuBar();
         mb.setMaximumSize(new Dimension(400, 30));
-        
         JMenu m1 = new JMenu("FILE");
         JMenu m2 = new JMenu("Help");
         mb.add(m1);
         mb.add(m2);
         JMenuItem mi1 = new JMenuItem("Add item");
         JMenuItem mi2 = new JMenuItem("Edit item");
+        
+        mi1.addActionListener(this);
+
         m1.add(mi1);
         m1.add(mi2);
+    }
 
-        JPanel date = new JPanel();
+    private void buildDate() {
+        date = new JPanel();
         date.setMaximumSize(new Dimension(400, 40));
         date.setBorder(BorderFactory.createLineBorder(Color.black));
         JButton prev = new JButton("←");
@@ -38,18 +49,35 @@ public class gui{
         date.add(prev);
         date.add(day);
         date.add(next);
+    }
 
-        JPanel lp = new JPanel();
+    private void buildChecklist() {
+        lp = new JPanel();
         lp.setLayout(new BorderLayout());
-        JPanel list = new JPanel();
+        list = new JPanel();
         list.setLayout(new BoxLayout(list, BoxLayout.PAGE_AXIS));
         list.setBorder(BorderFactory.createLineBorder(Color.black));
         
         list.add(new JCheckBox("Placeholder"));
         list.add(new JCheckBox("number 2"));
-
+    
         lp.add(list, BorderLayout.WEST);
         list.setPreferredSize(new Dimension(400, 330));
+    }
+
+    private void addListItem() {
+        list.add(new JCheckBox("New item"));
+    }
+
+    public gui() {
+        buildWindow();
+
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+
+        buildMenu();
+        buildDate();
+        buildChecklist();
 
         panel.add(mb);
         panel.add(date);
@@ -57,5 +85,17 @@ public class gui{
 
         frame.getContentPane().add(panel);
         frame.setVisible(true);
+    }
+    public static void main(String args[]) {
+        gui g = new gui();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+        if (e.getActionCommand() == "Add item") {
+            System.out.println("test");
+            addListItem();
+        }
     }
 }
